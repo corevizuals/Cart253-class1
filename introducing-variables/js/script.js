1793c7c96868fedca7d852cad06f2b028307baa1
corevizuals <96845976+corevizuals@users.noreply.github.com>
2024-10-03 15:07:15 -0400

    Used variables to make the program change!

--- introducing-variables/js/script.js
+++ introducing-variables/js/script.js
@@ -2,17 +2,16 @@
  * Title of Project
  * Author Name
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Learning what a variable is and does
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create a canvas
 */
 function setup() {
-
+    createCanvas(1000,480);
 }
 
 
@@ -20,5 +19,12 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    //Draw the circle
+    push ();
+    fill (mouseX, mouseY,0);
+    noStroke ();
+    ellipse (width / 2, height / 2, 100, 100);
+    pop ();
 }
