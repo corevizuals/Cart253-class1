02e2533bfe100f0cad57064362c494275cf5f69e
corevizuals <96845976+corevizuals@users.noreply.github.com>
2024-10-03 23:39:38 -0400

    Describing the Fun Circle project

--- ArtJam/js/script.js
+++ ArtJam/js/script.js
@@ -1,7 +1,18 @@
 /**
  * 
+ * Art Jam Project
  * 
  * 
- * 
- * 
+ * Fun Circles is an interactive art piece.
  */
+
+
+"use strict";
+
+let angle = 0;
+let circleSize;
+
+function setup() {
+  createCanvas(400, 400);
+  circleSize = width / 4;
+}
