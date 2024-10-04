d877f5d5bd0a8957d162f13ceb5c3238cf9319cb
corevizuals <96845976+corevizuals@users.noreply.github.com>
2024-10-03 23:43:47 -0400

    changing background color when mouse is moved

--- ArtJam/js/script.js
+++ ArtJam/js/script.js
@@ -16,3 +16,20 @@ function setup() {
   createCanvas(400, 400);
   circleSize = width / 4;
 }
+
+function draw() {
+  // Background color changes based on mouseX
+  // Movement: Horizontal mouse movement shifts background from dark to light
+  let bgColor = map(mouseX, 0, width, 0, 255);
+  background(bgColor, 220, 220);
+  
+  // Continuous animation of the central circle
+  // pulsing effect
+  angle += 0.02;
+  let oscillation = sin(angle) * 50;
+  
+  // Circle size changes based on mouseY
+  // Vertical mouse movement changes circle size
+  let mappedSize = map(mouseY, 0, height, circleSize / 2, circleSize * 2);
+
+}
